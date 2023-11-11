1a39f7e6bbedb1d7df54fafec6823ad873ba97d2
zeeshan ali <zeeshangondal0007@gmail.com>
2023-11-11 22:56:12 +0500

    c5

--- src/components/DoorScene.js
+++ src/components/DoorScene.js
@@ -8,14 +8,18 @@ extend({ MeshPhysicalMaterial, MeshStandardMaterial });
 
 function DoorScene(props) {
     let { sWidth, sHeight, doorHandleVisible, doorType } = props
+    var positionX=0
+    if(doorType===3){
+        positionX=0.8
+    }
     // Nested DoorHandle function
     function DoorHandle() {
-        const handleWidth = 0.04;
-        const handleHeight = 0.5;
+        const handleWidth = 0.08;
+        const handleHeight = 0.53;
         const handleDepth = 0.2;
-        const handlePositionX = -sWidth / 2 + handleWidth / 2;
+        const handlePositionX = positionX+sWidth/2 + handleWidth / 2 -handleWidth/3;
 
-        if (!doorHandleVisible) {
+        if (!doorHandleVisible || doorType === 4) {
             return null;
         }
 
@@ -27,8 +31,12 @@ function DoorScene(props) {
     }
 
     function GlassRectangle() {
+        let position=[0, 0, 0]
+        if(doorType===3){
+            position=[positionX,0,0]
+        }
         return (
-            <Box args={[sWidth, sHeight, 0.07]} position={[0, 0, 0]}>
+            <Box args={[sWidth, sHeight, 0.07]} position={[...position]}>
                 <meshPhysicalMaterial
                     attach="material"
                     color="lightgray"
@@ -51,54 +59,67 @@ function DoorScene(props) {
             frameDepth = 0.09;
         }
         if (doorType === 2) {
-            thickness = { left: 0.025, right: 0.025, top: 0.2, bottom: 0.2 }
+            thickness = { left: 0.04, right: 0.05, top: 0.2, bottom: 0.2 }
+            frameDepth = 0.07;
+        }
+        if (doorType === 3) {
+            thickness = { left: 0.05, right: 0.05, top: 0.15, bottom: 0.05 }
             frameDepth = 0.07;
         }
         if (doorType === 4) {
-            thickness = { left: 0.025, right: 0.025, top: 0.025, bottom: 0.025 }
+            thickness = { left: 0.05, right: 0.05, top: 0.05, bottom: 0.05 }
             frameDepth = 0.07;
         }
 
-
-        // if (doorType === 2) {
-        //     frameThickness = 0.08;
-        //     frameDepth = 0.08;
-        // }
         const TopFrame = () => {
             if (doorType === 1 || doorType === 4)
                 return <Box args={[sWidth + thickness.top * 2, thickness.top, frameDepth]}
-                    position={[0, sHeight / 2 + thickness.top / 2, 0]}>
+                    position={[positionX, sHeight / 2 + thickness.top / 2, 0]}>
                     <meshStandardMaterial color="black" />
                 </Box>
             if (doorType === 2)
-                return <Box args={[sWidth + thickness.top * 2 - 0.4, thickness.top, frameDepth]}
-                    position={[0, sHeight / 2 + thickness.top / 2 - thickness.top, 0]}>
+                return <Box args={[sWidth + thickness.top * 2 - 0.4, thickness.top, frameDepth + 0.01]}
+                    position={[positionX, sHeight / 2 + thickness.top / 2 - thickness.top, 0]}>
                     <meshStandardMaterial color="black" />
                 </Box>
+
+            if (doorType === 3) {
+                const topFramePosition = [0, sHeight / 2 + thickness.top / 2, 0]
+
+                return <Box args={[sWidth * 2 + thickness.top * 2, thickness.top, frameDepth + 0.01]}
+                    position={[...topFramePosition]}>
+                    <meshStandardMaterial color="black" />
+                </Box>
+            }
 
         }
 
         const BottomFrame = () => {
             if (doorType === 1 || doorType === 4)
                 return <Box args={[sWidth + thickness.bottom * 2, thickness.bottom, frameDepth]}
-                    position={[0, -sHeight / 2 - thickness.bottom / 2, 0]}>
+                    position={[positionX, -sHeight / 2 - thickness.bottom / 2, 0]}>
                     <meshStandardMaterial color="black" />
                 </Box>
             if (doorType === 2)
-                return <Box args={[sWidth + thickness.bottom * 2 - 0.4, thickness.bottom, frameDepth]}
-                    position={[0, -sHeight / 2 - thickness.bottom / 2 + thickness.bottom, 0]}>
+                return <Box args={[sWidth + thickness.bottom * 2 - 0.4, thickness.bottom, frameDepth + 0.01]}
+                    position={[positionX, -sHeight / 2 - thickness.bottom / 2 + thickness.bottom, 0]}>
                     <meshStandardMaterial color="black" />
                 </Box>
-            
+            if (doorType === 3)
+                return <Box args={[sWidth + thickness.bottom * 2, thickness.bottom, frameDepth]}
+                    position={[positionX, -sHeight / 2 - thickness.bottom / 2, 0]}>
+                    <meshStandardMaterial color="black" />
+                </Box>
+
         }
         const LeftFrame =
             <Box args={[thickness.left, sHeight, frameDepth]}
-                position={[-sWidth / 2 - thickness.left / 2, 0, 0]}>
+                position={[positionX+ -sWidth / 2 - thickness.left / 2, 0, 0]}>
                 <meshStandardMaterial color="black" />
             </Box>
         const RightFrame =
             <Box args={[thickness.right, sHeight, frameDepth]}
-                position={[sWidth / 2 + thickness.right / 2, 0, 0]}>
+                position={[positionX+sWidth / 2 + thickness.right / 2, 0, 0]}>
                 <meshStandardMaterial color="black" />
             </Box>
         return (
@@ -142,9 +163,8 @@ function DoorScene(props) {
             <ambientLight intensity={0.9} />
             <directionalLight ref={lightRef} position={[0, 0, 5]} intensity={2} color="white" />
             <directionalLight position={[-5, 0, -5]} intensity={1.5} color="white" />
-
-            <GlassRectangle />
             <Frame />
+            <GlassRectangle />
             <DoorHandle />
             <LimitedOrbitControls />
         </Canvas>
